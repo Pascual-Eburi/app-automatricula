2ba3621d8bbe513f281f7aa222346057d898781b
Pascual Eburi <pascualeburi@gmail.com>
2024-06-22 17:52:04 +0200

    Remove front files

--- src/helpers/Media.js
+++
@@ -1,8 +0,0 @@
-import { API_URL } from "../routers/Api";
-
-
-const DEFAULT_USER_AVATAR = "../../assets/media/avatars/default.png";
-
-export function UserAvatarUrl(photo){
-    return photo ? `${API_URL}/${photo}` : DEFAULT_USER_AVATAR
-}
